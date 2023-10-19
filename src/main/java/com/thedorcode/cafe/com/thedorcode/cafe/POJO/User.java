90aba78cbf9a2342f77c0af334e16e4a1fd37851
tshimsthedoor <tshims79@gmail.com>
2023-10-19 16:40:50 +0200

    we added the @Data annotation so that we can remove the constructor and getter and setter

--- src/main/java/com/thedorcode/cafe/com/thedorcode/cafe/POJO/User.java
+++ src/main/java/com/thedorcode/cafe/com/thedorcode/cafe/POJO/User.java
@@ -2,13 +2,14 @@ package com.thedorcode.cafe.com.thedorcode.cafe.POJO;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 
-
+@Data
 @Entity
 @DynamicInsert
 @Table(name="user")
@@ -40,70 +41,5 @@ public class User implements Serializable {
     @Column(name = "role")
     private String role;
 
-    public User(){}
 
-    public User(String name, String contactNumber, String email, String password, String status, String role) {
-        this.name = name;
-        this.contactNumber = contactNumber;
-        this.email = email;
-        this.password = password;
-        this.status = status;
-        this.role = role;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
