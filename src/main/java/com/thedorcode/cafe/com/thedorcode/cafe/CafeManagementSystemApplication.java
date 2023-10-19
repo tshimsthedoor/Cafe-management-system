05274eea9ddeea05af495b032b0faa4d8f3e215c
tshimsthedoor <tshims79@gmail.com>
2023-10-19 16:17:33 +0200

    We created the base template

---
+++ src/main/java/com/thedorcode/cafe/com/thedorcode/cafe/CafeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thedorcode.cafe.com.thedorcode.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemApplication.class, args);
+	}
+
+}
